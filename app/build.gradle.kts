51a1a042d8d6a40ff6b02eebfcf541795ce11b1f
sudi-3385 <sudipta.deb@zohocorp.com>
2026-03-20 23:21:02 +0530

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     // Core
